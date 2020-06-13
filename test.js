93cf2140803bd09157bb988645ef3b0b2931793c
Andrew Bennett <bennetaw@mcmaster.ca>
2020-06-13 09:41:30 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,70 @@
+document.write('<link rel="stylesheet" href="https://github.githubassets.com/assets/gist-embed-31007ea0d3bd9f80540adfbc55afc7bd.css">')
+document.write(`
+	<div id=\"gist103698241\" class=\"gist\">\n    
+		<div class=\"gist-file\">\n      
+			<div class=\"gist-data\">\n        
+				<div class=\"js-gist-file-update-container js-task-list-container file-box\">\n  
+					<div id=\"file-index-html\" class=\"file\">\n    \n\n  
+						<div itemprop=\"text\" class=\"Box-body p-0 blob-wrapper data type-html \">\n      \n
+							<table class=\"highlight tab-size js-file-line-container\" data-tab-size=\"8\" data-paste-markdown-skip>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L1\" class=\"blob-num js-line-number\" data-line-number=\"1\"><\/td>\n        
+									<td id=\"file-index-html-LC1\" class=\"blob-code blob-code-inner js-file-line\"><span class=pl-c1>&lt;!DOCTYPE html<span class=pl-kos>&gt;<\/span><\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L2\" class=\"blob-num js-line-number\" data-line-number=\"2\"><\/td>\n        
+									<td id=\"file-index-html-LC2\" class=\"blob-code blob-code-inner js-file-line\"><span class=pl-kos>&lt;<\/span><span class=pl-ent>html<\/span> <span class=pl-c1>lang<\/span>=&quot;<span class=pl-s>en<\/span>&quot;<span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L3\" class=\"blob-num js-line-number\" data-line-number=\"3\"><\/td>\n        
+									<td id=\"file-index-html-LC3\" class=\"blob-code blob-code-inner js-file-line\">  <span class=pl-kos>&lt;<\/span><span class=pl-ent>head<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L4\" class=\"blob-num js-line-number\" data-line-number=\"4\"><\/td>\n        
+									<td id=\"file-index-html-LC4\" class=\"blob-code blob-code-inner js-file-line\">    <span class=pl-kos>&lt;<\/span><span class=pl-ent>title<\/span><span class=pl-kos>&gt;<\/span>Sample page<span class=pl-kos>&lt;/<\/span><span class=pl-ent>title<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L5\" class=\"blob-num js-line-number\" data-line-number=\"5\"><\/td>\n        
+									<td id=\"file-index-html-LC5\" class=\"blob-code blob-code-inner js-file-line\">  <span class=pl-kos>&lt;/<\/span><span class=pl-ent>head<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L6\" class=\"blob-num js-line-number\" data-line-number=\"6\"><\/td>\n        
+									<td id=\"file-index-html-LC6\" class=\"blob-code blob-code-inner js-file-line\">  <span class=pl-kos>&lt;<\/span><span class=pl-ent>body<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L7\" class=\"blob-num js-line-number\" data-line-number=\"7\"><\/td>\n        
+									<td id=\"file-index-html-LC7\" class=\"blob-code blob-code-inner js-file-line\">    <span class=pl-kos>&lt;<\/span><span class=pl-ent>h1<\/span><span class=pl-kos>&gt;<\/span>Sample page<span class=pl-kos>&lt;/<\/span><span class=pl-ent>h1<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L8\" class=\"blob-num js-line-number\" data-line-number=\"8\"><\/td>\n        
+									<td id=\"file-index-html-LC8\" class=\"blob-code blob-code-inner js-file-line\">    <span class=pl-kos>&lt;<\/span><span class=pl-ent>p<\/span><span class=pl-kos>&gt;<\/span>This is a <span class=pl-kos>&lt;<\/span><span class=pl-ent>a<\/span> <span class=pl-c1>href<\/span>=&quot;<span class=pl-s>index.html<\/span>&quot;<span class=pl-kos>&gt;<\/span>simple<span class=pl-kos>&lt;/<\/span><span class=pl-ent>a<\/span><span class=pl-kos>&gt;<\/span> sample.<span class=pl-kos>&lt;/<\/span><span class=pl-ent>p<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L9\" class=\"blob-num js-line-number\" data-line-number=\"9\"><\/td>\n        
+									<td id=\"file-index-html-LC9\" class=\"blob-code blob-code-inner js-file-line\">    <span class=pl-c>&lt;!-- this is a comment --&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L10\" class=\"blob-num js-line-number\" data-line-number=\"10\"><\/td>\n        
+									<td id=\"file-index-html-LC10\" class=\"blob-code blob-code-inner js-file-line\">  <span class=pl-kos>&lt;/<\/span><span class=pl-ent>body<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n      
+								<tr>\n        
+									<td id=\"file-index-html-L11\" class=\"blob-num js-line-number\" data-line-number=\"11\"><\/td>\n        
+									<td id=\"file-index-html-LC11\" class=\"blob-code blob-code-inner js-file-line\"><span class=pl-kos>&lt;/<\/span><span class=pl-ent>html<\/span><span class=pl-kos>&gt;<\/span><\/td>\n      
+								<\/tr>\n
+							<\/table>\n\n\n  
+						<\/div>\n\n  
+					<\/div>\n
+				<\/div>\n\n      
+			<\/div>\n      
+			<div class=\"gist-meta\">\n        
+				<a href=\"https://gist.github.com/bnaut/9cb4f0ef7f5586a5ecc74d317bb53b30/raw/b6514b171b3db0d9f245c12bb62a254c97ed547c/index.html\" style=\"float:right\">view raw<\/a>\n        
+				<a href=\"https://gist.github.com/bnaut/9cb4f0ef7f5586a5ecc74d317bb53b30#file-index-html\">index.html<\/a>\n        hosted with &#10084; by <a href=\"https://github.com\">GitHub<\/a>\n      
+			<\/div>\n    
+		<\/div>\n
+	<\/div>\n`
+)
+
+
+
+
+
